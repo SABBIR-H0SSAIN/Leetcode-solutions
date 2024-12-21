fa048e0762e9afc074fe12ae7ac25e7cc654992e
Md. Sabbir Hossain <82496216+Its-sabbir@users.noreply.github.com>
2024-12-21 22:42:53 +0600

    Create 20003_Merge_k_Sorted_Lists.cpp

---
+++ 20003_Merge_k_Sorted_Lists.cpp
@@ -0,0 +1,53 @@
+// Author: Sabbir Hossain
+// Problem Link: https://leetcode.com/problems/merge-k-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ // @1st Approch ( Brute Force Solution )
+
+class Solution {
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        if(!list1 && !list2 ) return nullptr;
+        if(!list1) return list2;
+        if(!list2) return list1;
+        
+        ListNode *dummy= new ListNode(-1);
+        ListNode *ptr=dummy;
+        
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                ptr->next=list1;
+                list1=list1->next;
+            }else{
+                ptr->next=list2;
+                list2=list2->next;
+            }
+            ptr=ptr->next;
+        }
+        if(list1)ptr->next=list1;
+        else if(list2) ptr->next=list2;
+        return dummy->next;
+    }
+
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(!lists.size()) return nullptr;
+
+        ListNode *res=lists[0];
+        for(int i=1;i<lists.size();i++){
+           res = mergeTwoLists(res,lists[i]);
+        }
+        return res;
+    }
+};
