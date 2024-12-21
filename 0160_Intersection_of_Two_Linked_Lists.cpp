07a7aac0414b8945c67856903ae3a0e8364a0479
Md. Sabbir Hossain <82496216+Its-sabbir@users.noreply.github.com>
2024-12-22 03:08:38 +0600

    Added Optimized Approch

--- 0160_Intersection_of_Two_Linked_Lists.cpp
+++ 0160_Intersection_of_Two_Linked_Lists.cpp
@@ -31,3 +31,20 @@ public:
         return headA;
     }
 };
+
+// @2nd Approch ( Without Size Calculation )
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+
+        ListNode *ptr1 = headA;
+        ListNode *ptr2 = headB;
+
+        while(ptr1 != ptr2){
+            ptr1 = ptr1 ? ptr1->next : headB;
+            ptr2 = ptr2 ? ptr2->next : headA;
+        }
+        return ptr1;
+    }
+};
