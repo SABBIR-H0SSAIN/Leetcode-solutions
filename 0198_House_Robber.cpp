69ab5274568e883607bafdc168ea09718e1654a3
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-03-15 22:50:25 +0600

    Create 0198_House_Robber.cpp

---
+++ 0198_House_Robber.cpp
@@ -0,0 +1,35 @@
+// Author: Sabbir Hossain
+// Problem Link: https://leetcode.com/problems/house-robber/
+
+// @dp solution with space complexity O(n)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1){
+            return nums[0];
+        }
+
+        vector<int>dp(nums.size(),0);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+
+        for(int i=2;i<nums.size();i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.size()-1];
+    }
+};
+
+// @dp solution with O(1) space
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev=0,cur=0;
+        for(int i=0;i<nums.size();i++){
+            int tempa=max(cur,prev+nums[i]);
+            prev=cur;
+            cur=temp;
+        }
+        return cur;
+    }
+};
