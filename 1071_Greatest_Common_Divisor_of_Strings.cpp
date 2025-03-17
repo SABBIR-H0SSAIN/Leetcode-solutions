8a03f845126377178b934391fac353e4206894b0
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-03-18 02:00:18 +0600

    Update 1071_Greatest_Common_Divisor_of_Strings.cpp

--- 1071_Greatest_Common_Divisor_of_Strings.cpp
+++ 1071_Greatest_Common_Divisor_of_Strings.cpp
@@ -31,3 +31,22 @@ public:
         return "";
     }
 };
+
+// @Optimized approch using gcd calculation
+class Solution {
+public:
+    int gcd(int a,int b){
+        while(b != 0){
+            int temp=a%b;
+            a=b;
+            b=temp;
+        }
+        return a;
+    }
+
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2 != str2+str1) return "";
+
+        return str1.substr(0,gcd(str1.size(),str2.size()));
+    }
+};
