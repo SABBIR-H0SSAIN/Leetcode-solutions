b552e4055483029068af66b15e9e7e34954583c5
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-03-22 22:43:56 +0600

    Update 2685_Count_the_Number_of_Complete_Components.cpp

--- 2685_Count_the_Number_of_Complete_Components.cpp
+++ 2685_Count_the_Number_of_Complete_Components.cpp
@@ -54,6 +54,7 @@ public:
     }
 };
 
+
 // @DFS approch solution
 class Solution {
 public:
@@ -88,6 +89,76 @@ public:
             
             dfs(n,g,visited,v,e,i);
             count += (e==v*(v-1));
+        }
+        return count;
+    }
+};
+
+
+// @DSU approch solution
+class DSU {
+    vector<int> parent, size;
+
+public:
+    DSU(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findParent(int child) {
+        if (parent[child] == child) return child;
+        return parent[child] = findParent(parent[child]);
+    }
+
+    bool isConnected(int u, int v) {
+        return findParent(u) == findParent(v);
+    }
+
+    void connect(int u, int v) {
+        u = findParent(u);
+        v = findParent(v);
+
+        if (u != v) {
+            if (size[u] > size[v]) {
+                parent[v] = u;
+                size[u] += size[v];
+            } else {
+                parent[u] = v;
+                size[v] += size[u];
+            }
+        }
+    }
+
+    int getSize(int node) {
+        return size[findParent(node)];
+    }
+};
+
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        DSU dsu(n);
+        int count=0;
+        unordered_map<int,int>edgeCount;
+
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0],v=edges[i][1];
+            dsu.connect(u,v);
+        }
+        
+        for(int i=0;i<edges.size();i++){
+            edgeCount[dsu.findParent(edges[i][0])]++;
+        }
+ 
+        for(int i=0;i<n;i++){
+            if(dsu.findParent(i)==i){
+                int v=dsu.getSize(i);
+                int e=edgeCount[i];
+                count+=(e==v*(v-1)/2);
+            }
         }
         return count;
     }
