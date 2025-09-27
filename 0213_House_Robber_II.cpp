0166f4b4f48bb36b032bc22c24befe72d976005b
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-09-27 22:51:21 +0600

    Update 0213_House_Robber_II.cpp

--- 0213_House_Robber_II.cpp
+++ 0213_House_Robber_II.cpp
@@ -3,25 +3,22 @@
 
 class Solution {
 public:
-    int max_rob(vector<int>&nums,int l,int r){
-
-        int prev=nums[l];
-        int cur=max(nums[l],nums[l+1]);
-
-        for(int i=l+2;i<r;i++){
-            int temp=max(cur,prev+nums[i]);
-            prev=cur;
-            cur=temp;
-        }
-        return cur;
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=3) return *max_element(nums.begin(),nums.end());
+        return max(maxRob(nums,0,n-1),maxRob(nums,1,n));
     }
 
-    int rob(vector<int>& nums) {
+    int maxRob(vector<int>& nums,int l,int r) {
+        int prev=nums[l];
+        int curr=max(nums[l],nums[l+1]);
 
-        int n=nums.size();
-        if(n==1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
+        for(int i=l+2;i<r;i++){
+            int temp=curr;
+            curr= max(curr,prev+nums[i]);
+            prev=temp;
+        }
 
-        return max(max_rob(nums,0,n-1),max_rob(nums,1,n));
+        return curr;
     }
 };
