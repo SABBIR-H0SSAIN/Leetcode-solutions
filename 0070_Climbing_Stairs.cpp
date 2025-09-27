49bfc12e946472739761c49625297feeeca7e695
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-09-27 18:42:23 +0600

    Update 0070_Climbing_Stairs.cpp

--- 0070_Climbing_Stairs.cpp
+++ 0070_Climbing_Stairs.cpp
@@ -29,3 +29,19 @@ public:
         return dp[n];
     }
 };
+
+// O(1) Space solution
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<3) return n;
+        int prev=1,curr=2;
+        for(int i=3;i<=n;i++) {
+            int temp=curr;
+            curr=prev+curr;
+            prev=temp;
+
+        }
+        return curr;
+    }
+};
