919d6ee4fb8e33cc2aab789e814acc3e296988a5
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-09-28 11:06:54 +0600

    Update 0152_Maximum_Product_Subarray.cpp

--- 0152_Maximum_Product_Subarray.cpp
+++ 0152_Maximum_Product_Subarray.cpp
@@ -20,3 +20,21 @@ public:
         return ans;
     }
 };
+
+// More cleaner version
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans=nums[0];
+        int mini=nums[0],maxi=nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] < 0) swap(mini,maxi);
+
+            maxi=max(maxi*nums[i],nums[i]);
+            mini=min(mini*nums[i],nums[i]);
+            ans=max(ans,maxi);
+        }
+        return ans;
+    }
+};
